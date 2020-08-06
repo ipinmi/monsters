ba57bf75c6fe0c0520b2b296abafcd085ab6c49d
ipinmi <ipinmiadebayo@gmail.com>
2020-08-06 13:40:52 +0100

    Updates

---
+++ precache-manifest.4a474d9f2229c86c21c67a318a9d8b21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a000bbd0593578b25cb81d4119bcb7a6",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "8ee3144a875562557610",
+    "url": "/monsters/static/css/main.ebe41d72.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/monsters/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ee3144a875562557610",
+    "url": "/monsters/static/js/main.ebb7490a.chunk.js"
+  },
+  {
+    "revision": "55328b6a8e96452d341b",
+    "url": "/monsters/static/js/runtime-main.3a250f91.js"
+  }
+]);
